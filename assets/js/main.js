096a8403a450e75f570c8b55f2631c9b73624ecf
yoesk8 <yoelgutierrez2021@yahoo.com>
2022-07-15 21:35:34 +0000

    Added bitcoin price websocket with Binance Api

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,3 +112,10 @@ const questions = [
     ]
   }
 ]
+//Websocket for live bitcoin price
+let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcgbp@trade');
+let stockPriceElement = document.getElementById('stock-price');
+ws.onmessage = (event) => {
+  let stockObject = JSON.parse(event.data);
+  stockPriceElement.innerText = parseFloat(stockObject.p).toFixed(2);
+}
